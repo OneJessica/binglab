8124466fc0d3f08132b65c05c73ff87142ede062
zhangjin <54884562+OneJessica@users.noreply.github.com>
2026-03-17 20:35:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_data(file = '2025-06-25T09-07_export（old）.csv'):
     return antibody_df
 st.header('Antibody')
 filename =st.multiselect('Check file',glob('*.csv'),)
-antibody_df = get_data(filename)
+antibody_df = get_data(filename,default='2025-06-25T09-07_export（old）.csv')
 num_row = st.sidebar.selectbox(label='编辑',options=('fixed','dynamic'))
 tab1,tab2,tab3 = st.tabs(['🔍查询','📕抗体详情','统计'])
 with tab1:
