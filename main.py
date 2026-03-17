9d8317eeee3c34b2fd178a89bf5ba2789f35c26a
zhangjin <54884562+OneJessica@users.noreply.github.com>
2026-03-17 21:01:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ def read_update_log():
     with open('update.log.txt',) as f:
         text = f.readlines()
         return '\n'.join(text)
+
+
+def get_data(file = 'binglab_antibody.v0.0.8.csv'):
+    antibody_df= pd.read_csv(file,encoding='latin1',index_col=0)
+    return antibody_df
+    
 st.header('Antibody')
 filename =st.selectbox('Check file',glob('*.csv'),)
 antibody_df = get_data(filename,)
 # from pandas.testing import assert_frame_equal
 # @st.cache_data
 
-def get_data(file = 'binglab_antibody.v0.0.8.csv'):
-    antibody_df= pd.read_csv(file,encoding='latin1',index_col=0)
-    return antibody_df
 if 'df' not in st.session_state:
     st.session_state['df'] = antibody_df
 
